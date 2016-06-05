4b47b5a674e54302cd15ba8476fe8d9ebe0f4b1c
Simon Chaffetz <simonc@uber.com>
2016-06-04 17:26:44 -0700

    Add particles floating toward vanishing point

--- sketch.js
+++ sketch.js
@@ -43,6 +43,38 @@ function animateParticles () {
         particle.move();
         particle.render();
     });
+    line(640, 480, mouseX + RECT_WIDTH, mouseY + RECT_HEIGHT);
+    line(
+        displayWidth,
+        displayHeight,
+        mouseX + RECT_WIDTH,
+        mouseY + RECT_HEIGHT
+    );
+
+    animateParticles();
+}
+
+function animateParticles () {
+    particles.forEach(function (particle) {
+        particle.move();
+        particle.render();
+    });
+}
+
+function mousePressed() {
+    fullscreen(true);
+    addParticle();
+}
+
+function addParticle () {
+    var particleIdx = particles.length;
+
+    // Approximation.
+    // Should be equal to angle of line from center and bottom of screen to
+    // center of rectangle.
+    var particleAngle = (mouseX + RECT_WIDTH / 2) - displayWidth / 2;
+    var particle = new Particle(particleIdx, particleAngle);
+    particles.push(particle);
 }
 
 function mousePressed() {
@@ -96,3 +128,38 @@ Particle.prototype._destroy = function () {
     particles.splice(this.index);
 }
 
+
+function Particle (index, angle) {
+    this.xPos = displayWidth / 2;
+    this.yPos = displayHeight;
+    this.width = 25;
+    this.height = 50;
+
+    this.index = index;
+    this.upVelocity = 4;
+    this.sideVelocity = angle * .005;
+}
+
+Particle.prototype.move = function () {
+    this.yPos -= this.upVelocity;
+    this.xPos += this.sideVelocity;
+
+    var didReachRectMiddle = this.yPos <= mouseY + RECT_HEIGHT / 2;
+    if (didReachRectMiddle) {
+        this._destroy();
+    }
+    this._shrink();
+};
+
+Particle.prototype.render = function () {
+    ellipse(this.xPos, this.yPos, this.height, this.width);
+}
+
+Particle.prototype._shrink= function () {
+    this.height *= .99;
+    this.width *= .99;
+}
+
+Particle.prototype._destroy = function () {
+    particles.splice(this.index);
+}
